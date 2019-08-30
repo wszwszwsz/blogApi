870fc02cd23d2c3259ea6638ade47eb0160f9223
Wojtek <szypelt@seo-partner.pl>
2019-08-30 15:53:20 +0200

    Login function part2

---
+++ src/Form/UserLoginType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserLoginType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('password' textt)
+            ->add('name')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
+
+
+->add('title', TextType::class, [
+    'label' => 'Tytuł',
+])
+    ->add('content', TextareaType::class, [
+        'label' => 'Treść',
+        'attr' => [
+            'placeholder' => 'Tu wpisz tekst posta'
+        ]
+    ])
+    ->add('submit', SubmitType::class)
+;
